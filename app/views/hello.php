c455d6d773e33debaf3e91bf637f5cf342794adc
Stefan Zweifel <stef.zweifel@gmail.com>
2014-05-23 20:59:56 +0200

    Init Repo
    Signed-off-by: Stefan Zweifel <stef.zweifel@gmail.com>

---
+++ app/views/hello.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			color: #999;
+		}
+
+		.welcome {
+			width: 300px;
+			height: 200px;
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			margin-left: -150px;
+			margin-top: -100px;
+		}
+
+		a, a:visited {
+			text-decoration:none;
+		}
+
+		h1 {
+			font-size: 32px;
+			margin: 16px 0 0 0;
+		}
+	</style>
+</head>
+<body>
+	<div class="welcome">
+		<h1>Screenly</h1>
+		<p>API to get Screenshots from Websites</p>
+	</div>
+</body>
+</html>
