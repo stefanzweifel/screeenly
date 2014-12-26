12da734a492e5181e1e08c11f5b5634e8b674139
Stefan Zweifel <hello@stefanzweifel.io>
2014-12-26 21:15:20 +0100

    Trigger error in api.auth

--- app/filters.php
+++ app/filters.php
@@ -85,7 +85,7 @@ Route::filter('csrf', function()
 Route::filter('api.auth', function(){
 
     if( !User::getUserByKey( Input::get('key') ) ) {
-        return Response::json('Access denied.', 401, ['Access-Control-Allow-Origin' => '*']);
+        return App::abort(401, 'Access denied.', ['Access-Control-Allow-Origin' => '*']);
     }
 
 });
