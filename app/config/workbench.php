0fa13f5e0cbbdccb570b37d0384dc483f282a644
Stefan Zweifel <hello@stefanzweifel.io>
2015-01-13 19:57:52 +0100

    Fixed workbench.php

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'name' => '',
+	'name' => 'Stefan Zweifel',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
 	|
 	*/
 
-	'email' => '',
+	'email' => 'hello@stefanzweifel.io',
 
 );
