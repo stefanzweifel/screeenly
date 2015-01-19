ae6e5314654c8ce8106747a809c375f6b4935533
Stefan Zweifel <hello@stefanzweifel.io>
2015-01-19 20:28:22 +0100

    Added some example pictures

--- app/views/layouts/landingpage.blade.php
+++ app/views/layouts/landingpage.blade.php
@@ -1,10 +1,7 @@
 {{-- Include Header (Styles, Meta Tags and more) --}}
 @include('layouts.partials._header')
 
-
 @yield('content')
-
-
 
 {{-- include tail (scripts and end tags) --}}
 @include('layouts.partials._tail')
