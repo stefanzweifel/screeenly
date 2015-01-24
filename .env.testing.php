b76c897efd4149896acf031bbb99dc7585cb481e
Stefan Zweifel <hello@stefanzweifel.io>
2015-01-24 12:15:19 +0100

    Added env-variables for testing
    Travis should work now.

---
+++ .env.testing.php
@@ -0,0 +1,33 @@
+<?php
+
+    return array(
+
+        /**
+         * Github OAuth Configuration
+         */
+        'github_client_id' => 'CLIENT-ID',
+        'github_secret'    => 'GITHUB-SECRET',
+
+        /**
+         * Laravel Encryption Key
+         */
+        'encryption_key'   => 'SUPER-SECRET-ENCRYPTION-KEY',
+
+        /**
+         * Database Configuration
+         */
+        'db_host'          => 'localhost',
+        'db_name'          => 'database',
+        'db_user'          => 'homestead',
+        'db_pass'          => 'secret',
+
+        /**
+         * Slack Configuration
+         */
+        'slack_channel'    => '#general',
+        'slack_token'      => 'SECRET-WEBHOOK-TOKEN',
+        'slack_bot'        => 'Screeenly',
+        'slack_domain'     => 'YOUR-SLACK-DOMAIN',
+        'slack_icon'       => ':ghost:',
+
+    );
