04b2a39173be0f11af9928e9641872f8b4d56310
Stefan Zweifel <hello@stefanzweifel.io>
2015-02-17 21:26:08 +0100

    Moved Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,21 +1,81 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
+Route::get('/', [
+    'as' => 'home.landingpage',
+    'uses' => 'PagesController@showLandingpage'
 ]);
+
+Route::get('terms', [
+    'as' => 'front.terms',
+    'uses' => 'PagesController@showTerms'
+]);
+
+Route::get('imprint', [
+    'as' => 'front.imprint',
+    'uses' => 'PagesController@showImprint'
+]);
+
+Route::get('login', array(
+    'as'   => 'login',
+    'uses' => 'OAuthController@redirectToProvider')
+);
+
+Route::get('handle', array(
+    'as' => 'login_handler',
+    'uses' => 'OAuthController@handleProviderCallback'
+));
+
+Route::get('logout', array(
+    'as'     => 'oauth.logout',
+    'uses'   => 'OAuthController@logout'
+));
+
+Route::get('try', array(
+    'as'   => 'try',
+    'uses' => 'PagesController@showTestingForm'
+));
+
+Route::post('try', array(
+    'as'     => 'try.do',
+    'middleware' => 'csrf',
+    'uses'   => 'PagesController@createTestScreenshot'
+));
+
+/**
+ * Account Routes
+ */
+Route::group(['middleware' => 'auth'], function(){
+
+    Route::get('dashboard', array(
+        'as' => 'front.dashboard',
+        'uses' => 'PagesController@showDashboard'
+    ));
+
+    Route::post('reset', array(
+        'as' => 'front.resetAPIKey',
+        'uses' => 'UserController@resetAPIKey'
+    ));
+
+    Route::post('close', array(
+        'as' => 'front.closeAccount',
+        'uses' => 'UserController@closeAccount'
+    ));
+
+});
+
+
+/**
+ * API Routes
+ */
+Route::group(['prefix' => 'api', 'middleware' => 'api.auth|api.throttle'], function(){
+
+    Route::group(['prefix' => 'v1'], function(){
+
+        Route::post('fullsize', array(
+            'as'   => 'api.fullsize',
+            'uses' => 'APIController@createScreenshot'
+        ));
+
+    });
+
+});
