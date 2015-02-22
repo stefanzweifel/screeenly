4cfddf7e305f74f354aed82215134a029628f32f
Stefan Zweifel <hello@stefanzweifel.io>
2015-02-22 15:54:51 +0100

    Added SoftDeletes to User Model

--- app/User.php
+++ app/User.php
@@ -6,9 +6,13 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
-	use Authenticatable, CanResetPassword;
+	use Authenticatable, CanResetPassword, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 
 	/**
 	 * The database table used by the model.
