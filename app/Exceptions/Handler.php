2741f4159bfdb27562c1ba9b9f1784c4f5e7da22
Stefan Zweifel <hello@stefanzweifel.io>
2015-02-22 11:56:03 +0100

    Tabs => Spaces

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,43 +5,43 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler {
 
-	/**
-	 * A list of the exception types that should not be reported.
-	 *
-	 * @var array
-	 */
-	protected $dontReport = [
-		'Symfony\Component\HttpKernel\Exception\HttpException'
-	];
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        'Symfony\Component\HttpKernel\Exception\HttpException'
+    ];
 
-	/**
-	 * Report or log an exception.
-	 *
-	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-	 *
-	 * @param  \Exception  $e
-	 * @return void
-	 */
-	public function report(Exception $e)
-	{
-		return parent::report($e);
-	}
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $e
+     * @return void
+     */
+    public function report(Exception $e)
+    {
+        return parent::report($e);
+    }
 
-	/**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $e
-	 * @return \Illuminate\Http\Response
-	 */
-	public function render($request, Exception $e)
-	{
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $e
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $e)
+    {
         $code = 0;
 
-		/**
-		 * Handle API Errors
-		 */
-		if ( $request->is('api/*') && $request->isMethod('post') ) {
+        /**
+         * Handle API Errors
+         */
+        if ( $request->is('api/*') && $request->isMethod('post') ) {
 
             if (method_exists($e, 'getHeaders')) {
                 $headers = $e->getHeaders();
@@ -52,7 +52,7 @@ class Handler extends ExceptionHandler {
             }
 
             if ($code == 0) {
-            	$code = 400;
+                $code = 400;
             }
 
             \Log::error($e);
@@ -65,20 +65,12 @@ class Handler extends ExceptionHandler {
                 'message' => $e->getMessage()
             );
 
-			return \Response::json($returnMessage, $code, $headers);
+            return \Response::json($returnMessage, $code, $headers);
 
-		}
+        }
 
-		return parent::render($request, $e);
+        return parent::render($request, $e);
 
-
-
-
-		// if ($e instanceof Exception) {
-
-		//     return redirect()->route('home.landingpage')->withErrors(['error' => $e->getMessage()]);
-		// }
-
-	}
+    }
 
 }
