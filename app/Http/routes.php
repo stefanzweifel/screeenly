57db72e16eeb8e0952fd41d1a6dc4846f1f92199
Stefan Zweifel <hello@stefanzweifel.io>
2016-01-21 21:46:52 +0100

    Fix Routes File

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,42 +1,44 @@
 <?php
 
-Route::get('/', ['as' => 'home.landingpage', 'uses' => 'PagesController@showLandingpage']);
-Route::get('terms', ['as' => 'front.terms', 'uses' => 'StaticController@showTerms']);
-Route::get('imprint', ['as' => 'front.imprint', 'uses' => 'StaticController@showImprint']);
-Route::get('donate', ['as' => 'front.donate', 'uses' => 'StaticController@showDonate']);
+Route::group(['middleware' => 'web'], function () {
 
-Route::get('login', ['as' => 'login', 'uses' => 'OAuthController@redirectToProvider']);
-Route::get('handle', ['as' => 'login_handler', 'uses' => 'OAuthController@handleProviderCallback']);
-Route::get('logout', ['as' => 'oauth.logout', 'uses' => 'OAuthController@logout']);
+    Route::get('/', ['as' => 'home.landingpage', 'uses' => 'PagesController@showLandingpage']);
+    Route::get('terms', ['as' => 'front.terms', 'uses' => 'StaticController@showTerms']);
+    Route::get('imprint', ['as' => 'front.imprint', 'uses' => 'StaticController@showImprint']);
+    Route::get('donate', ['as' => 'front.donate', 'uses' => 'StaticController@showDonate']);
 
-Route::get('try', ['as' => 'try', 'uses' => 'PagesController@showTestingForm']);
+    Route::get('login', ['as' => 'login', 'uses' => 'OAuthController@redirectToProvider']);
+    Route::get('handle', ['as' => 'login_handler', 'uses' => 'OAuthController@handleProviderCallback']);
+    Route::get('logout', ['as' => 'oauth.logout', 'uses' => 'OAuthController@logout']);
 
-Route::post('try', [
-    'as' => 'try.do',
-    'middleware' => 'web',
-    'uses' => 'PagesController@createTestScreenshot',
-]);
+    Route::get('try', ['as' => 'try', 'uses' => 'PagesController@showTestingForm']);
 
-Route::get('feedback', ['as' => 'app.feedback', 'uses' => 'StaticController@showFeedback']);
-Route::get('faq', ['as' => 'app.faq', 'uses' => 'StaticController@showFaq']);
+    Route::post('try', [
+        'as' => 'try.do',
+        'uses' => 'PagesController@createTestScreenshot',
+    ]);
 
-/*
- * Account Routes
- */
-Route::group(['middleware' => ['web', 'auth']], function () {
+    Route::get('feedback', ['as' => 'app.feedback', 'uses' => 'StaticController@showFeedback']);
+    Route::get('faq', ['as' => 'app.faq', 'uses' => 'StaticController@showFaq']);
 
-    Route::get('dashboard', array(
-        'as'         => 'app.dashboard',
-        'uses'       => 'PagesController@showDashboard',
-        'middleware' => 'app.hasEmail',
-    ));
 
-    Route::resource('apikeys', 'ApiKeysController', ['except' => ['index', 'create']]);
 
-    Route::delete('close', ['as' => 'app.closeAccount', 'uses' => 'UserController@closeAccount']);
-    Route::get('email-setup', ['as' => 'app.storeEmailForm', 'uses' => 'PagesController@showEmailForm']);
-    Route::post('email-setup', ['as' => 'app.storeEmail', 'uses' => 'UserController@storeEmail']);
-    Route::get('settings', ['as' => 'app.settings', 'uses' => 'PagesController@showSettings']);
+    Route::group(['middleware' => 'auth'], function () {
+
+        Route::get('dashboard', [
+            'as'         => 'app.dashboard',
+            'uses'       => 'PagesController@showDashboard',
+            'middleware' => 'app.hasEmail',
+        ]);
+
+        Route::resource('apikeys', 'ApiKeysController', ['except' => ['index', 'create']]);
+
+        Route::delete('close', ['as' => 'app.closeAccount', 'uses' => 'UserController@closeAccount']);
+        Route::get('email-setup', ['as' => 'app.storeEmailForm', 'uses' => 'PagesController@showEmailForm']);
+        Route::post('email-setup', ['as' => 'app.storeEmail', 'uses' => 'UserController@storeEmail']);
+        Route::get('settings', ['as' => 'app.settings', 'uses' => 'PagesController@showSettings']);
+
+    });
 
 });
 
