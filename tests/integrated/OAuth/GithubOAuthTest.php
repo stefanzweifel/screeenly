fb7eeee183ac2fcd2f914a2a0eaf67753fe449ff
Stefan Zweifel <hello@stefanzweifel.io>
2016-01-30 19:57:23 +0100

    Add GithubOAuthTest
    Currently unused. How could I test the oauth flow?

---
+++ tests/integrated/OAuth/GithubOAuthTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class GithubOAuthTest extends TestCase
+{
+    use DatabaseMigrations, DatabaseTransactions;
+
+    /** @test */
+    public function it_redirects_to_github()
+    {
+        /**
+         * How could it test this?
+         */
+
+        // $response = $this->visit("/login");
+        // $this->assertRedirectedTo("http://github.com");
+    }
+
+}
