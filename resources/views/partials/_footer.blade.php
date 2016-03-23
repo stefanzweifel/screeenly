8bba0c9dabc7f617582b17a8f1b33da65d42db45
Stefan Zweifel <hello@stefanzweifel.io>
2016-03-23 20:05:45 +0100

    Add Bugsnag Logo

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,36 +1,47 @@
-<footer class="white bg-black">
-  <div class="px2 py3 container">
-    <div class="clearfix mxn2">
-      <div class="col col-6 md-col-3">
-        <ul class="list-reset">
-          <li><a href="/feedback" class="h5 btn block">Feedback</a></li>
-          <li><a href="/faq" class="h5 btn block">FAQ</a></li>
-          <li><a href="//github.com/stefanzweifel/screeenly/wiki" class="h5 btn block">Docs</a></li>
-          <li><a href="/donate" class="h5 btn block">Donate</a></li>
-        </ul>
-      </div>
-      <div class="col col-6 md-col-3">
-        <ul class="list-reset">
-          <li><a href="/imprint" class="h5 btn block">Imprint</a></li>
-          <!-- <li><a href="/privacy" class="h5 btn block">Privacy</a></li> -->
-          <li><a href="/terms" class="h5 btn block">Terms of Service</a></li>
-        </ul>
-      </div>
-      <div class="col col-6 md-col-3">
-        <ul class="list-reset">
-          <li><a href="https://github.com/stefanzweifel/screeenly" class="h5 btn block">Github</a></li>
-          <li><a href="https://github.com/stefanzweifel/screeenly/issues" class="h5 btn block">Issues</a></li>
-        </ul>
-      </div>
-      <div class="col col-6 md-col-3">
-        <ul class="list-reset">
-          <li><a href="https://twitter.com/screeenly" class="h5 btn block">Twitter</a></li>
-          <li><a href="http://blog.screeenly.com" class="h5 btn block">Blog</a></li>
-          <li><a href="mailto:hello@screeenly.com" class="h5 btn block">Email</a></li>
-        </ul>
-      </div>
-
+@if (auth()->guest())
+    <div class="white bg-blue">
+        <div class="p2 container center">
+            <p>Sponsored by</p>
+            <a href="http://busgnag.com" target="blank">
+                <img src="/assets/images/bgsnag-logo.png" class="" style="max-width: 200px;" alt="">
+            </a>
+        </div>
     </div>
-    <p class="h6 mb0">&copy; {{ date('Y')}} screeenly.com</p>
-  </div>
+@endif
+
+<footer class="white bg-black">
+
+    <div class="px2 py3 container">
+        <div class="clearfix mxn2">
+            <div class="col col-6 md-col-3">
+                <ul class="list-reset">
+                    <li><a href="/feedback" class="h5 btn block">Feedback</a></li>
+                    <li><a href="/faq" class="h5 btn block">FAQ</a></li>
+                    <li><a href="//github.com/stefanzweifel/screeenly/wiki" class="h5 btn block">Docs</a></li>
+                    <li><a href="/donate" class="h5 btn block">Donate</a></li>
+                </ul>
+            </div>
+            <div class="col col-6 md-col-3">
+                <ul class="list-reset">
+                    <li><a href="/imprint" class="h5 btn block">Imprint</a></li>
+                    <li><a href="/terms" class="h5 btn block">Terms of Service</a></li>
+                </ul>
+            </div>
+            <div class="col col-6 md-col-3">
+                <ul class="list-reset">
+                    <li><a href="https://github.com/stefanzweifel/screeenly" class="h5 btn block">Github</a></li>
+                    <li><a href="https://github.com/stefanzweifel/screeenly/issues" class="h5 btn block">Issues</a></li>
+                </ul>
+            </div>
+            <div class="col col-6 md-col-3">
+                <ul class="list-reset">
+                    <li><a href="https://twitter.com/screeenly" class="h5 btn block">Twitter</a></li>
+                    <li><a href="http://blog.screeenly.com" class="h5 btn block">Blog</a></li>
+                    <li><a href="mailto:hello@screeenly.com" class="h5 btn block">Email</a></li>
+                </ul>
+            </div>
+
+        </div>
+        <p class="h6 mb0">&copy; {{ date('Y')}} screeenly.com</p>
+    </div>
 </footer>
