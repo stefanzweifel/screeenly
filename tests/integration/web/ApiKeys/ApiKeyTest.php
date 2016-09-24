1331a419668965ae18f2f5e5810a8d07a18bfe22
Stefan Zweifel <hello@stefanzweifel.io>
2016-09-24 22:06:46 +0200

    Move ApiKeyTest

---
+++ tests/integration/web/ApiKeys/ApiKeyTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use App\Models\ApiKey;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ApiKeyTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function login($override = [])
+    {
+        return $this->actingAs(factory(User::class)->create($override));
+    }
+
+    /** @test */
+    public function it_lists_api_keys_a_user_created()
+    {
+        $user = factory(User::class)->create();
+        $apiKey = factory(ApiKey::class)->create([
+            'user_id' => $user->id,
+            'name' => 'This is my test API Key'
+        ]);
+
+        $this->actingAs($user)
+            ->visit('/dashboard')
+            ->see("This is my test API Key");
+    }
+
+    /** @test */
+    public function it_shows_message_if_no_api_keys_exists()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+            ->visit('/dashboard')
+            ->see("You currently doesn't have an API key setup");
+    }
+
+    /** @test */
+    public function it_lets_user_create_a_new_api_key()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+            ->visit('/dashboard')
+            ->see("Name for your new API Key")
+            ->type('FooBar', 'name')
+            ->press('Create API Key')
+            ->seePageIs('/dashboard')
+            ->see("Your new API Key has been created.");
+
+
+        $this->seeInDatabase('api_keys', [
+            'name' => 'FooBar',
+            'user_id' => $user->id
+        ]);
+    }
+
+    /** @test */
+    public function it_lets_user_delete_an_api_key()
+    {
+        $user = factory(User::class)->create();
+        $apiKey = factory(ApiKey::class)->create(['user_id' => $user->id]);
+
+        $this->actingAs($user)
+            ->visit('/dashboard')
+            ->press('Delete')
+            ->seePageIs('/dashboard')
+            ->see("API Key destroyed.");
+
+        $this->assertNull(ApiKey::find($apiKey->id));
+    }
+}
