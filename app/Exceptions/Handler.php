5f5b49985c715f8726f5cfc43a9e52b16450418d
Stefan Zweifel <hello@stefanzweifel.io>
2017-01-18 20:47:19 +0100

    Add filp/whoops for prettier exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -88,4 +88,26 @@ class Handler extends ExceptionHandler
 
         return redirect()->guest('login');
     }
+
+    /**
+     * Create a Symfony response for the given exception.
+     *
+     * @param  \Exception  $e
+     * @return mixed
+     */
+    protected function convertExceptionToResponse(Exception $e)
+    {
+        if (config('app.debug')) {
+            $whoops = new \Whoops\Run;
+            $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+
+            return response()->make(
+                $whoops->handleException($e),
+                method_exists($e, 'getStatusCode') ? $e->getStatusCode() : 500,
+                method_exists($e, 'getHeaders') ? $e->getHeaders() : []
+            );
+        }
+
+        return parent::convertExceptionToResponse($e);
+    }
 }
