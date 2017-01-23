eb716c9030aa67fe052250852c5b2668fb91a8ee
Stefan Zweifel <hello@stefanzweifel.io>
2017-01-23 21:24:36 +0100

    Update Mail Config

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'no-reply@screeenly.com',
+        'name' => 'screeenly',
     ],
 
     /*
