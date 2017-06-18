1fd1605a0f454a71f63baa4a82a5eb14f2b1d6ba
Stefan Zweifel <hello@stefanzweifel.io>
2017-06-18 20:54:16 +0200

    Add deploy.php
    Add Configuration File for [deployer](https://deployer.org/).
    
    I've switched from my current deployment solution to "deployer". This is the configuration file used to push code to live and production server.
    (Note the two new ENV-Variables used for the deployment).
    
    You can start a deployment by running `dep deploy live|stage`

---
+++ deploy.php
@@ -0,0 +1,38 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+
+require __DIR__ . '/vendor/autoload.php';
+$dotenv = new \Dotenv\Dotenv(__DIR__ . "/");
+$dotenv->load();
+
+// Configuration
+
+set('repository', 'git@github.com:stefanzweifel/screeenly.git');
+set('git_tty', true); // [Optional] Allocate tty for git on first deployment
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('screeenly.com')
+    ->stage('production')
+    ->set('deploy_path', getenv('DEPLOY_PATH_PRODUTION'));
+
+host('stage.screeenly.com')
+    ->stage('stage')
+    ->set('deploy_path', getenv('DEPLOY_PATH_STAGE'));
+
+
+// Tasks
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+// before('deploy:symlink', 'artisan:migrate');
