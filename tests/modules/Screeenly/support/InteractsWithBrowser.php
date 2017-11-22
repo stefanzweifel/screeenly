97387634b7e66a9719dcab5f0516ad4f2d6414aa
Stefan Zweifel <hello@stefanzweifel.io>
2017-11-22 19:41:55 +0100

    Remove References to PhantomJs

--- tests/modules/Screeenly/support/InteractsWithBrowser.php
+++ tests/modules/Screeenly/support/InteractsWithBrowser.php
@@ -1,14 +1,14 @@
 <?php
 
-use Screeenly\Entities\Screenshot;
-use Screeenly\Services\PhantomsJsBrowser;
 use Screeenly\Contracts\CanCaptureScreenshot;
+use Screeenly\Entities\Screenshot;
+use Screeenly\Services\ChromeBrowser;
 
 trait InteractsWithBrowser
 {
     public function replaceBinding()
     {
-        $stub = $this->createMock(PhantomsJsBrowser::class);
+        $stub = $this->createMock(ChromeBrowser::class);
         $stub->expects($this->any())
             ->method('capture')
             ->willReturn(new Screenshot(storage_path('testing/test-screenshot.jpg')));
