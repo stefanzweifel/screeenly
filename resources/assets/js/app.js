e0130798857e8f31a2f315c56d884d4019e6d62f
Stefan Zweifel <hello@stefanzweifel.io>
2018-04-02 15:43:29 +0200

    Remove unused NPM dependencies

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,15 +6,3 @@
  */
 
 require('./bootstrap');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the body of the page. From here, you may begin adding components to
- * the application, or feel free to tweak this setup for your needs.
- */
-
-// Vue.component('example', require('./components/Example.vue'));
-
-// const app = new Vue({
-//     el: 'body'
-// });
