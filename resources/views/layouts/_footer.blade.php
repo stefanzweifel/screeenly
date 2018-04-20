bd14eb7af6c55042f8ffaf4ab1728f8c0fd6e252
Stefan Zweifel <hello@stefanzweifel.io>
2018-04-20 20:56:24 +0200

    Rename Link in Footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,7 +2,7 @@
     <ul class="list-inline">
         <li><a href="/imprint">Imprint</a></li>
         <li><a href="/terms">Terms of Service</a></li>
-        <li><a href="/about">About</a></li>
+        <li><a href="/faq">FAQ</a></li>
         <li class="hidden-xs">|</li>
         <li>Project by <a href="https://stefanzweifel.io" target="_blank" rel="noopener noreferrer">Stefan Zweifel</a></li>
         <li><a href="https://github.com/stefanzweifel/screeenly" target="_blank" rel="noopener noreferrer">Source Code</a></li>
