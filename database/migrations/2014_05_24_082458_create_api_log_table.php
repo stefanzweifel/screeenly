cf5e56235c73a871a2ad219217b91c9f9ed4e7c9
Stefan Zweifel <hello@stefanzweifel.io>
2018-08-11 21:58:17 +0200

    Remove a foreign key constraint from a migration

--- database/migrations/2014_05_24_082458_create_api_log_table.php
+++ database/migrations/2014_05_24_082458_create_api_log_table.php
@@ -16,8 +16,9 @@ class CreateApiLogTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->nullable()->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
+
             $table->integer('api_key_id')->nullable();
-            $table->foreign('api_key_id')->references('id')->on('api_keys');
+
             $table->text('images');
             $table->timestamps();
         });
