361247bcad7efcfd0dc381a0e9eeedbfd5a15b92
Stefan Zweifel <hello@stefanzweifel.io>
2019-06-17 20:12:06 +0200

    Install sentry

---
+++ config/sentry.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'dsn' => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
+
+    // capture release as git sha
+    // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
+
+    'breadcrumbs' => [
+
+        // Capture bindings on SQL queries logged in breadcrumbs
+        'sql_bindings' => true,
+
+    ],
+
+];
