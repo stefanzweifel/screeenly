eaa68f81ce2ee0e9030bd21746ec7de705c63aa3
Stefan Zweifel <hello@stefanzweifel.io>
2020-08-17 20:42:55 +0200

    Rewrite EmailAuthRegisterTest

--- tests/app/integration/web/Auth/EmailAuthRegisterTest.php
+++ tests/app/integration/web/Auth/EmailAuthRegisterTest.php
@@ -1,17 +1,19 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Screeenly\Models\User;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
 
-class EmailAuthRegisterTest extends BrowserKitTestCase
+class EmailAuthRegisterTest extends TestCase
 {
-    use DatabaseTransactions;
+    use RefreshDatabase;
 
     /** @test */
     public function it_loads_register_view()
     {
-        $this->visit('register')
-            ->assertResponseOk();
+        $response = $this->get('/register');
+
+        $response->assertOk();
     }
 
     /** @test */
@@ -19,29 +21,31 @@ class EmailAuthRegisterTest extends BrowserKitTestCase
     {
         $user = factory(User::class)->create();
 
-        $this->visit('register')
-            ->type('foo', 'name')
-            ->type($user->email, 'email')
-            ->type('Password1234', 'password')
-            ->type('Password1234', 'password_confirmation')
-            ->press('Register')
-            ->seePageIs('/register')
-            ->see('The email has already been taken.');
+        $response = $this->post('/register', [
+            'name' => 'foo',
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertSessionHasErrors([
+            'email' => 'The email has already been taken.'
+        ]);
     }
 
     /** @test */
     public function it_registers_new_user_and_redirects_him_to_dashboard()
     {
-        $this->visit('register')
-            ->type('foo', 'name')
-            ->type('foo@domain.com', 'email')
-            ->type('Password1234', 'password')
-            ->type('Password1234', 'password_confirmation')
-            ->press('Register')
-            ->seePageIs('/dashboard')
-            ->see('Logout');
+        $response = $this->post('/register', [
+            'name' => 'foo',
+            'email' => 'foo@domain.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
 
-        $this->seeInDatabase('users', [
+        $response->assertRedirect('/dashboard');
+
+        $this->assertDatabaseHas('users', [
             'email' => 'foo@domain.com',
         ]);
     }
@@ -49,13 +53,15 @@ class EmailAuthRegisterTest extends BrowserKitTestCase
     /** @test */
     public function it_throws_error_if_password_does_not_match()
     {
-        $this->visit('register')
-            ->type('foo', 'name')
-            ->type('foo@bar.com', 'email')
-            ->type('Password1234', 'password')
-            ->type('SomethingElse', 'password_confirmation')
-            ->press('Register')
-            ->seePageIs('/register')
-            ->see('The Password Confirmation does not match.');
+        $response = $this->post('/register', [
+            'name' => 'foo',
+            'email' => 'foo@domain.com',
+            'password' => 'password',
+            'password_confirmation' => 'password_not_match',
+        ]);
+
+        $response->assertSessionHasErrors([
+            'password',
+        ]);
     }
 }
