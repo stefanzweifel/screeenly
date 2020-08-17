9106ab85a98d67fcd6ecba47281259ee62d70ede
Stefan Zweifel <hello@stefanzweifel.io>
2020-08-17 20:53:06 +0200

    Add testing filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'testing' => [
+            'driver' => 'local',
+            'root' => storage_path('testing'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
