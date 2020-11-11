b750ce8803f91e5b2476d75a21824fb19e8afee2
Stefan Zweifel <hello@stefanzweifel.io>
2020-11-11 19:18:42 +0100

    Simplify Terms of Service

--- modules/Screeenly/Resources/views/static/terms.blade.php
+++ modules/Screeenly/Resources/views/static/terms.blade.php
@@ -7,49 +7,34 @@
     <div class="panel panel-default">
         <div class="panel-heading">Terms of Service</div>
         <div class="panel-body">
+            <p>Last updated: November 11, 2020</p>
 
-            <p>Last updated: August 04, 2015</p>
+            <p>Use of the screeenly.com website and all associated services is offered subject to agreement to the
+                following terms of service.</p>
 
-            <p>Please read these Terms of Service ("Terms", "Terms of Service") carefully before using the http://screeenly.com website (the "Service") operated by Screeenly ("us", "we", or "our").</p>
-            <p>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
-            <p>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.</p>
+            <h2 class="h3">No Warranty</h2>
 
-            <h4>Accounts</h4>
+            <p>All services are provided &quot;as is&quot; without any warranty of any kind including but not limited to
+                fitness for a particular purpose.</p>
 
-            <p>When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.</p>
-            <p>You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service.</p>
-            <p>You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.</p>
+            <h2 class="h3">Fair Usage</h2>
 
-            <h4>Links To Other Web Sites</h4>
+            <p>screeenly.com provides a <b>free</b> API to capture screenshots of publicly available websites. Please
+                keep this in mind when implementing our API in your application .</p>
+            <p><b>DO NOT</b> issue thousands of HTTP requests in a short amount of time. If we detect that your IP
+                address is creating too much strain on our servers, we will block your IP address.</p>
 
-            <p>Our Service may contain links to third-party web sites or services that are not owned or controlled by Screeenly.</p>
-            <p>Screeenly has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Screeenly shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
-            <p>We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.</p>
+            <h2 class="h3">Abuse</h2>
 
-            <h4>Termination</h4>
+            <p>We reserve the right to take action in response to reported abuse of our services. This action may
+                include - but is not limited to - the deletion of API tokens or other account details. Abuse may include
+                - but is not limited to - any action which is illegal under the city, state, or federal laws where you
+                are currently present. This includes copyright infringement under the DMCA or any activity which we deem
+                disruptive.</p>
 
-            <p>We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</p>
-            <p>All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
-            <p>We may terminate or suspend your account immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</p>
-            <p>Upon termination, your right to use the Service will immediately cease. If you wish to terminate your account, you may simply discontinue using the Service.</p>
-            <p>All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+            <h2 class="h3">Modification of Agreement</h2>
 
-            <h4>Governing Law</h4>
-
-            <p>These Terms shall be governed and construed in accordance with the laws of Switzerland, without regard to its conflict of law provisions.</p>
-
-            <p>Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
-
-            <h4>Changes</h4>
-
-            <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
-            <p>By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service.</p>
-            <p>Our Terms of Service agreement was created by TermsFeed.</p>
-
-            <h4>Contact Us</h4>
-
-            <p>If you have any questions about these Terms, please contact us.</p>
-
+            <p>screeenly reserves the right to modify this agreement at any time without prior notice.</p>
         </div>
 
     </div>
