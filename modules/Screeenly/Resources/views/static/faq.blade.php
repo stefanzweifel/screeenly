e2b7fae3728691e54edf2d77268b5e3d98d5ec3b
Stefan Zweifel <hello@stefanzweifel.io>
2020-11-11 19:44:28 +0100

    Update FAQ

--- modules/Screeenly/Resources/views/static/faq.blade.php
+++ modules/Screeenly/Resources/views/static/faq.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'About')
+@section('title', 'FAQ')
 
 @section('content')
 
@@ -8,12 +8,25 @@
     <div class="panel-heading">FAQ</div>
     <div class="panel-body">
 
+        <h3>Why is screeenly.com sometimes down or otherwise not available?</h3>
+        <p>
+            screeenly is hosted on a relatively cheap server. Due to the fact that screeenly is a free service, a lot of people are using screeenly (which makes me very proud) and can sometimes lead to an increased load on our servers.
+        </p>
+        <p>
+            As screeenly is a free service and a side project of mine which doesn't generate any money for me, I can't upgrade the servers too much, as I would loose too much money.
+        </p>
+        <p>
+            If you want to support screeenly consider switching to a paid plan on our <a href="https://3.screeenly.com">newest version</a>, a
+            <a href="https://github.com/sponsors/stefanzweifel">Sponsorship on GitHub</a> or
+            <a href="https://buymeacoff.ee/3oQ64YW">buy me a coffee</a>. All donations will help pay the server bills and allows me to invest more time into the service.
+        </p>
+
         <h3>I have a problem. How can I reach you?</h3>
         <p>
             The best way is to send me an email at <a href="mailto:hello@stefanzweifel.io">hello@stefanzweifel.io</a> or @ me at <a href="http://twitter.com/@_stefanzweifel">@_stefanzweifel</a> or <a href="http://twitter.com/@screeenly">@screeenly</a> on Twitter.
         </p>
         <p>
-            Due to the fact that this is just a sideproject I can't guarantee an answer in the next 5 minutes (I usually answer in the next 24 hours)
+            Due to the fact that this is just a side project I can't guarantee an answer in the next 5 minutes (I usually answer in the next 24 hours)
         </p>
 
         <h3>Is this service free?</h3>
@@ -21,7 +34,7 @@
             Yes, screeenly is a free service. No hidden fees.
         </p>
 
-        <h3>What data is beeing stored when using the service?</h3>
+        <h3>What data is being stored when using the service?</h3>
 
         <h4>Creating a new account (with email and password)</h4>
 
@@ -57,7 +70,7 @@
 
         <h3>Where is screeenly.com hosted?</h3>
         <p>
-            The application is currendly hosted on a <a href="https://digitalocean.com">Digital Ocean</a> Droplet in the FRA1 region.
+            The application is currently hosted on a <a href="https://digitalocean.com">Digital Ocean</a> Droplet in the FRA1 region.
         </p>
 
         <h3>Why does this service exist?</h3>
