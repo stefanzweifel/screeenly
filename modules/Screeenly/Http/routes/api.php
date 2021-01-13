3ae87593269f60c734112004e3208ae2f9185c42
Stefan Zweifel <hello@stefanzweifel.io>
2021-01-13 19:53:23 +0100

    Import Controllers in Route Files

--- modules/Screeenly/Http/routes/api.php
+++ modules/Screeenly/Http/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use Screeenly\Http\Controllers\Api\v1\ScreenshotController as ScreenshotControllerV1;
+use Screeenly\Http\Controllers\Api\v2\ScreenshotController as ScreenshotControllerv2;
+
 Route::group(['prefix' => 'v1'], function () {
-    Route::post('fullsize', 'v1\ScreenshotController@store')->middleware('auth:api');
+    Route::post('fullsize', [ScreenshotControllerV1::class, 'store'])->middleware('auth:api');
 });
 
 Route::group(['prefix' => 'v2'], function () {
-    Route::post('screenshot', 'v2\ScreenshotController@store')->middleware('auth:api');
+    Route::post('screenshot', [ScreenshotControllerv2::class, 'store'])->middleware('auth:api');
 });
